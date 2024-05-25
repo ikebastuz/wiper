1feefb7adf0230e640aec4e222656897da45a280
Alexandr Kobrin <ikebastuz@gmail.com>
2024-05-25 11:06:51 +1000

    added build scripts (#26)

---
+++ build/add_toolchain.sh
@@ -0,0 +1,7 @@
+rustup target add x86_64-apple-darwin    # for macOS Intel
+rustup target add aarch64-apple-darwin   # for macOS ARM
+rustup target add x86_64-pc-windows-gnu  # for Windows
+rustup target add x86_64-unknown-linux-gnu  # for Linux
+# Alternatively for linux - https://github.com/messense/homebrew-macos-cross-toolchains
+
+
